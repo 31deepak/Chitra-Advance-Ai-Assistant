c7bc820fcc3ec2f2a9a86baf80e264fac00680eb
31deepak <165395959+31deepak@users.noreply.github.com>
2025-03-17 14:58:36 +0530

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import eel
-
+import subprocess
+from fastapi import FastAPI
 from engine.features import *
 from engine.command import *
 from engine.auth import recoganize
-def start():
-    
-    eel.init("www")
 
+app = FastAPI()
+
+eel.init("www")
+
+def start():
     playAssistantSound()
+    
     @eel.expose
     def init():
         subprocess.call([r'device.bat'])
@@ -19,11 +23,20 @@ def start():
             eel.hideFaceAuth()
             speak("Face Authentication Successful")
             eel.hideFaceAuthSuccess()
-            speak("Hello, Welcome Sir, How can i Help You")
+            speak("Hello, Welcome Sir, How can I Help You")
             eel.hideStart()
             playAssistantSound()
         else:
             speak("Face Authentication Fail")
+    
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+    eel.start('index.html', mode=None, host='localhost', block=False)
 
-    eel.start('index.html', mode=None, host='localhost', block=True)
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    start()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
