01d7a6ea268f4ec2b00f4f48e7643c75d5b3a352
31deepak <165395959+31deepak@users.noreply.github.com>
2025-03-19 14:25:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
+git clone https://github.com/31deepak/Chitra-Advance-Ai-Assistant.git /MissPerfectURLBOT
 cd /MissPerfectURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 bot.py
+python3 main.py
